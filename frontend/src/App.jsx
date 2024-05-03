37570d8c73084264af08eea7e80a21733c9caff6
Rupeshkumar23 <coolrupesh2000@gmail.com>
2024-05-03 15:16:24 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
