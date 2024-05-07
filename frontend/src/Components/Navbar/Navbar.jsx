02f63ed4d32bddb0afdb8d3bff95276ae035a277
Rupeshkumar23 <coolrupesh2000@gmail.com>
2024-05-08 01:37:17 +0530

    updated code

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,31 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
+import { StoreContext } from '../../Context/StoreContext';
 
 
-const Navbar = () => {
-    const [menu,setMenu]=useState("home");
+const Navbar = ({setShowLogin}) => {
+    const [menu,setMenu]=useState("menu");
+    const {getTotalCartAmount}=useContext(StoreContext);
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt='logo' className='logo'/>
+       <Link to="/"> <img src={assets.logo} alt='logo' className='logo'/></Link>
         <ul className='navbar_menu'>
-            <li  onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
-            <li  onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-            <li  onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-            <li  onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+            <Link to="/"  onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+            <a href='#explore_menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+            <a  href='#app_download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+            <a  href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
         </ul>
         <div className="navbar_right">
-            <img src={assets.search_icon} alt="search_icon" />
+           <img src={assets.search_icon} alt="search_icon" />
             <div className="navbar_search_icon">
-                <img src={assets.basket_icon} alt="basket_icon" />
-                <div className="dot"></div>
+            <Link to="/cart">   <img src={assets.basket_icon} alt="basket_icon" /></Link>
+                <div className={getTotalCartAmount()===0?"":"dot"}></div>
             </div>
-            <button>sign in</button>
+            <button onClick={()=>setShowLogin(true)}>sign in</button>
         </div>
 
         </div>
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 export const StoreContext =createContext(null)
 
@@ -19,10 +19,17 @@ const StoreContexProvider =(props)=>{
        
             setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
-    useEffect(()=>{
-        console.log(cartItems)
-    },[cartItems])
-
+   
+const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for(const item in cartItems){
+        if(cartItems[item]>0){
+        let itemInfo = food_list.find((product)=> product._id === item)
+        totalAmount += itemInfo.price * cartItems[item];
+        }
+    }
+    return totalAmount;
+}
 
 
     const contextValue={
@@ -30,7 +37,8 @@ const StoreContexProvider =(props)=>{
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
     return(
         <StoreContext.Provider value={contextValue}>
