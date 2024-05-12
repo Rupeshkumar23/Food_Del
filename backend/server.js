237bf37d768a5beecf76dd8a66d0b96940258289
Rupeshkumar23 <coolrupesh2000@gmail.com>
2024-05-13 03:18:49 +0530

    updated code

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = 4000;
 app.use(express.json());
 app.use(cors(
     {
-        origin: true, // allow all origins
+        origin: "https://fooddel-rupeshkumar23s-projects.vercel.app", // allow all origins
         methods: ["GET", "POST"],
         credentials:true
     }
