a557c5f41273c54f6da98ebbdc66272b36a18579
Rupeshkumar23 <coolrupesh2000@gmail.com>
2024-05-16 01:03:30 +0530

    updated code

--- frontend/src/Components/AppDownload/AppDownload.jsx
+++ frontend/src/Components/AppDownload/AppDownload.jsx
@@ -9,15 +9,22 @@ const AppDownload = () => {
   const { theme } = useContext(ThemeContext);
   const darkModeClass = theme === 'dark' ? 'dark' : '';
   return (
-    <div className={`app_download ${darkModeClass}`} id="app_download">
+    <div className={`app_download_container ${darkModeClass}`} id="app_download">
+      <div className="app_download_content">
         <p>For Better Experience Download <br/>FreshBite App</p>
         <img className='img_mobile' src={mobile} width={200} alt="mobile" />
-    <div className="app_download_platforms">
-        <img src={assets.play_store} alt="play" />
-        <img src={assets.app_store} alt="app" />
-    </div>
+        <div className="app_download_platforms">
+          <img src={assets.play_store} alt="play" />
+          <img src={assets.app_store} alt="app" />
+        </div>
+      </div>
+      <div className="app_download_video">
+        <video autoPlay loop muted width="100%">
+          <source src={assets.vid_1} type="video/mp4" />
+        </video>
+      </div>
     </div>
   )
 }
 
-export default AppDownload
+export default AppDownload;
