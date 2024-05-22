d10c3a0a5d27f0717d992707b287135cdc9cd9a3
Rupeshkumar23 <coolrupesh2000@gmail.com>
2024-05-22 23:20:07 +0530

    updated code

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <div className={`navbar ${darkModeClass}`}>
         <img className='logo' src={assets.logo} alt="logo" />
         <h3><i className="fa-solid fa-user-tie" style={{color: "#ff6347",marginRight:"5px"}}></i>Admin panel</h3>
-        <img className='profile' src={assets.profile_image} alt="profile" />
+        <img className='profile' src={assets.user_profile} alt="profile" />
     </div>
   )
 }
